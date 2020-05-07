be2fc0a35262dc79dc7126d1cadd85bef9a335d0
= <muluk.dharmayana@gmail.com>
2020-05-08 06:46:12 +0700

    ubah jadi https di jxURL

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@ console.log('ok');
 function cariFilms(){
     $("#daftarfilm").html('');
     $.ajax({
-        ajaxURL: 'http://www.omdbapi.com/',
+        ajaxURL: 'https://www.omdbapi.com/',
         type: 'get',
         dataType: 'json',
         data: {
