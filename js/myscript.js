d319191df1f5261824ed927ac53dddfc20060505
= <muluk.dharmayana@gmail.com>
2020-05-08 07:12:52 +0700

    hapus ajax

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@ console.log('ok');
 function cariFilms(){
     $("#daftarfilm").html('');
     $.ajax({
-        url: 'https://www.omdbapi.com/',
+        url: 'http://www.omdbapi.com/',
         type: 'get',
         dataType: 'json',
         data: {
